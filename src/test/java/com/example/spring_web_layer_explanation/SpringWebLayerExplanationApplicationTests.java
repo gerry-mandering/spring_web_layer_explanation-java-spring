c73a6889c2cee6a681652cb94fb0d62fabf61745
gerry-mandering <kimmen1119@gmail.com>
2023-09-08 16:38:25 +0900

    Initial commit

---
+++ src/test/java/com/example/spring_web_layer_explanation/SpringWebLayerExplanationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_web_layer_explanation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebLayerExplanationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
